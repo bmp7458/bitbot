9ab404aee8a78158c14c19aa05de18b3b319b9bc
bmp <124109360+bmp7458@users.noreply.github.com>
2023-02-04 19:13:38 -0500

    quotes command

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
 const Bot = require('anony-mpp-client');
 const prefix = 'b'
-const commands = ["help", "about", "ping", "whoami", "myrole", "eval", "echo"]
+const commands = ["help", "about", "ping", "whoami", "myrole", "eval", "echo", "quote"]
 const roles = ["Bot Owner", "Administator", "Beta Bot Tester"]
 const http = require('http')
 const fs = require('fs')
 const indivroles = require('./roles.json')
+const axios = require("axios");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+function run(code) {
+  if (new String(code) == "[object Object]" == false && new String(code) == "[object JSON]" == false) {
+    try {
+      return '► ' + eval(code);
+    } catch (error) {
+      return '► ❌ ' + error + ''
+    }
+  } if (new String(code) == "[object Object]" || new String(code) == "[object JSON]") {
+    try { return '► ' + JSON.stringify(eval(code)); } catch (error) {
+      return '► ❌ ' + error + ' '
+    }
+  }
+}
+
+function getQuote() {
+  return fetch("https://zenquotes.io/api/random")
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      return data[0]["q"] + " -" + data[0]["a"]
+    })
+}
+
+
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('bitbot http server');
 }).listen(4040)
 
@@ -24,9 +50,9 @@ const bitbot = new Bot({
 
 function checkrole(id, role) {
   if (msg.p._id === id) {
-      bitbot.chat(msg.p.name + ', you have the role: ' + roles[0])
-    } else {
-      bitbot.chat(msg.p.name + ', You do not appear to have any roles!')
+    bitbot.chat(msg.p.name + ', you have the role: ' + roles[0])
+  } else {
+    bitbot.chat(msg.p.name + ', You do not appear to have any roles!')
   }
 }
 
@@ -36,43 +62,51 @@ bitbot.client.on('a', msg => {
     bitbot.chat(`Commands: (${commands.length}) - ${prefix}` + commands.join(`, ${prefix}`))
   }
 
-  if (cmd ===`${prefix}about`) {
+  if (cmd === `${prefix}about`) {
     bitbot.chat('**bitbot** is a multipurpose bot made to assist users in a regular fashion. **bitbot** created by ๖ۣۜb͜m͡p7458.')
   }
-  
+
   if (cmd === `${prefix}ping`) {
     ping = Number(Date.now()) - Number(msg.t)
     bitbot.chat("Latency is " + ping + "ms.")
   }
 
+  if (cmd === `${prefix}quote`) {
+    bitbot.chat(getQuote().then(quote => bitbot.chat(quote)))
+  }
+
   if (cmd === `${prefix}echo`) {
     bitbot.chat(msg.a.split(" ").splice(1).join(" "))
   }
+  /*
+    if (cmd === `${prefix}eval`) { //
+      bitbot.chat(run(msg.a.substr(cmd.length).trim()))
+     //i wanna fix some of your eval code
+    }
+    */
 
   if (cmd === `${prefix}eval`) {
-    bitbot.chat("> " + eval(msg.a.split(" ").splice(1).join(" ")))
-  }
-  
-  if (cmd === `${prefix}whoami`) {
-    bitbot.chat(msg.p.name + " | ID: " + msg.p._id + ' | Color: '+ msg.p.color )
-  }
-  if (cmd === `${prefix}myrole`) {
-    if (msg.p._id === "cedd04479c7028963568f1c0") {
-      bitbot.chat(msg.p.name + ', you have the role: ' + roles[0])
-    } else {
-      if (msg.p._id === "d59ff25d610cd21593d5a00c") {
-        bitbot.chat(msg.p.name + ', you have the role: ' + roles[2])
+    if (msg.p._id !== "cedd04479c7028963568f1c0") { bitbot.chat("No permissions!"); return; } else {
+      bitbot.chat(run(msg.a.substr(cmd.length).trim()))
+    }
+
+    if (cmd === `${prefix}whoami`) {
+      bitbot.chat(msg.p.name + " | ID: " + msg.p._id + ' | Color: ' + msg.p.color)
+    }
+    if (cmd === `${prefix}myrole`) {
+      if (msg.p._id === "cedd04479c7028963568f1c0") {
+        bitbot.chat(msg.p.name + ', you have the role: ' + roles[0])
       } else {
-        if (msg.p._id === "b28741a5c8548c845db04982") {
+        if (msg.p._id === "d59ff25d610cd21593d5a00c") {
           bitbot.chat(msg.p.name + ', you have the role: ' + roles[2])
         } else {
-          bitbot.chat(msg.p.name + ', You do not appear to have any roles!')
+          if (msg.p._id === "b28741a5c8548c845db04982") {
+            bitbot.chat(msg.p.name + ', you have the role: ' + roles[2])
+          } else {
+            bitbot.chat(msg.p.name + ', You do not appear to have any roles!')
+          }
         }
       }
     }
   }
 });
-
-
-
-eval(process.env.TOKEN="nope.")
